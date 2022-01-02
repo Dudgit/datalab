a9cfea57eb9ac08503ff58beef854ec6c53f2f41
Dudás Bence <bence.dudas20@gmail.com>
2022-01-02 16:56:20 +0100

    primitive node count

--- src/dude_pkg/functions.py
+++ src/dude_pkg/functions.py
@@ -1,9 +1,30 @@
+from pandas import merge
+features = ["trip_id","route_id","stop_sequence","stop_id",
+#"arrival_time","departure_time"
+"direction_id"
+]
 def stop_id_calculator(df):
     df.sort_values("stop_sequence",inplace = True)
     return df.stop_id
 
-# Might not need, cause of valuceounts
-#def route_parser(df):
-#    
-#    for ri in  df.route_id.unique():
-#    
+def mgm(df1,df2): # -> pd.Dataframe
+    """
+    Description:  
+    ------------  
+
+    My merge function. Merge 2 dataframes as pandas merge.
+
+    Args:  
+    ----  
+    - df1: pandas DataFrame, trips or stop_times  
+    - df2: pandas DataFrame, trips or stop_times (different from df1)  
+
+    Return:  
+    ------  
+
+    Merged dataframe with specific features.  
+
+    
+    """
+    tmp = merge(df1,df2,on = "trip_id")
+    return tmp.loc[:,features] 
