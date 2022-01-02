8ed7642d1b345eecf0739a4e7653bc959466c477
Dudás Bence <bence.dudas20@gmail.com>
2022-01-02 16:32:01 +0100

    task_2_3

--- src/dude_pkg/functions.py
+++ src/dude_pkg/functions.py
@@ -1,3 +1,9 @@
-# %%
-def foo():
-    print("__init__.py is working")
+def stop_id_calculator(df):
+    df.sort_values("stop_sequence",inplace = True)
+    return df.stop_id
+
+# Might not need, cause of valuceounts
+#def route_parser(df):
+#    
+#    for ri in  df.route_id.unique():
+#    
