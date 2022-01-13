c943f0a86fc3e19e2defa462d940972c6695a9ce
Bence Dudás <bence.dudas20@gmail.com>
2022-01-13 19:53:00 +0100

    gold line addded

--- src/dude_pkg/functions.py
+++ src/dude_pkg/functions.py
@@ -1,3 +1,4 @@
+from turtle import color
 from pandas import merge
 import pandas as pd
 import networkx as nx
@@ -6,7 +7,6 @@ import matplotlib
 from pandas.core.frame import DataFrame
 
 features = ["trip_id","route_id","stop_sequence","stop_id",
-#"arrival_time","departure_time"
 "direction_id"
 ]
 def create_network_data(table,snames): # -> pd.DataFrame
